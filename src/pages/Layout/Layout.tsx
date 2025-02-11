9f3885e7c0a906175676eb143cef2de7a9fa04fc
LIANAKES <lianakes@gmail.com>
2025-02-11 12:56:23 +0100

    init project

---
+++ src/pages/Layout/Layout.tsx
@@ -0,0 +1,97 @@
+import { useNavigate } from "react-router-dom"
+
+import { LayoutProps } from "./types"
+import {
+  LayoutWrapper,
+  Header,
+  HeaderLogoContainer,
+  HeaderLogo,
+  NavContainer,
+  Main,
+  Footer,
+  StyledNavLink,
+  StyledLink,
+  FooterNavContainer,
+} from "./styles"
+import Logo from '../../../assets/logo.jpg';
+
+
+function Layout({ children }: LayoutProps) {
+  const navigate = useNavigate()
+
+  const goToHomePage = () => navigate("/")
+
+  return (
+    <LayoutWrapper>
+      <Header>
+        <HeaderLogoContainer onClick={goToHomePage}>
+          <HeaderLogo src={Logo} />
+        </HeaderLogoContainer>
+        <NavContainer>
+          <StyledNavLink
+            style={({ isActive }) => ({
+              textDecoration: isActive ? "underline" : "none",
+            })}
+            to="/"
+          >
+            Home
+          </StyledNavLink>
+          <StyledNavLink
+            style={({ isActive }) => ({
+              textDecoration: isActive ? "underline" : "none",
+            })}
+            to="/users"
+          >
+            Users
+          </StyledNavLink>
+          <StyledNavLink
+            style={({ isActive }) => ({
+              textDecoration: isActive ? "underline" : "none",
+            })}
+            to="/lesson17"
+          >
+            Lesson 17
+          </StyledNavLink>
+          <StyledNavLink
+            style={({ isActive }) => ({
+              textDecoration: isActive ? "underline" : "none",
+            })}
+            to="/homework17"
+          >
+            Homework 17
+          </StyledNavLink>
+          <StyledNavLink
+            style={({ isActive }) => ({
+              textDecoration: isActive ? "underline" : "none",
+            })}
+            to="/lesson18"
+          >
+            Lesson 18
+          </StyledNavLink>
+          <StyledNavLink
+          style={({ isActive }) => ({
+            textDecoration: isActive ? "underline" : "none",
+          })}
+          to="/homework18"
+        >
+          Homework 18
+        </StyledNavLink>
+        </NavContainer>
+      </Header>
+      <Main>{children}</Main>
+      <Footer>
+        <HeaderLogoContainer onClick={goToHomePage}>
+          <HeaderLogo src={Logo} />
+        </HeaderLogoContainer>
+        <FooterNavContainer>
+          <StyledLink to="/">Home</StyledLink>
+          <StyledLink to="/users">Users</StyledLink>
+        </FooterNavContainer>
+      </Footer>
+    </LayoutWrapper>
+  )
+}
+
+export default Layout
+
+
